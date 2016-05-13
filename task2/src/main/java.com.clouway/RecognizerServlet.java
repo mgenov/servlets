652cebcb874a3a9e6bcd48c47d315a46b82ffea2
kristiyanpetkov <kristiqn.l.petkov@gmail.com>
2016-05-13 16:39:59 +0300

    ready with task2

---
+++ task2/src/main/java.com.clouway/RecognizerServlet.java
@@ -0,0 +1,34 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by clouway on 13.05.16.
+ */
+
+public class RecognizerServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    resp.setContentType("text/html");
+
+    String pageName = req.getParameter("page");
+
+    PrintWriter out = resp.getWriter();
+
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head><meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
+    out.println("<title>Session Test Servlet</title></head><body>");
+    out.println("<h2 style=\"color:blue;\">Request from: " + pageName + " page </h2>");
+    out.println("</body></html>");
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    doGet(req,resp);
+  }
+}
