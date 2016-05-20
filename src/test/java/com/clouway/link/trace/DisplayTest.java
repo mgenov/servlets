251bbfaa18ab51037594e9a93fa5481683fc42d3
KrasimirRaikov <raikov.krasimir@gmail.com>
2016-05-20 17:22:06 +0300

    renamed the page in the test to firstPage.

--- src/test/java/com/clouway/link/trace/DisplayTest.java
+++ src/test/java/com/clouway/link/trace/DisplayTest.java
@@ -50,7 +50,7 @@ public class DisplayTest {
   public void tracesRequestOrigin() throws Exception {
     context.checking(new Expectations() {{
       oneOf(request).getHeader("referer");
-      will(returnValue("page"));
+      will(returnValue("firstPage"));
 
       oneOf(response).setContentType("text/html");
 
@@ -62,6 +62,6 @@ public class DisplayTest {
     printWriter.flush();
 
     String result = outputStream.toString();
-    assertThat(result, containsString("<h1>page</h1>"));
+    assertThat(result, containsString("<h1>firstPage</h1>"));
   }
 }
