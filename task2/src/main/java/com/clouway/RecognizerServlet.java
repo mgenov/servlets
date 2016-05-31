27315584aba25d92b9540dd350eb2b3fe4382d90
kristiyanpetkov <kristiqn.l.petkov@gmail.com>
2016-05-31 15:03:03 +0300

    made servelt recognize only trusted pages

--- task2/src/main/java/com/clouway/RecognizerServlet.java
+++ task2/src/main/java/com/clouway/RecognizerServlet.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by com.clouway on 13.05.16.
@@ -15,7 +19,12 @@ import java.io.PrintWriter;
 public class RecognizerServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    String requestURL = req.getHeader("Referer");
+    Set<String> trustedPages = new HashSet<String>();
+    trustedPages.add("Abv");
+    trustedPages.add("Yahoo");
+    trustedPages.add("Gmail");
+
+    String requestedPage = req.getParameter("page");
 
     PrintWriter out = resp.getWriter();
 
@@ -23,7 +32,11 @@ public class RecognizerServlet extends HttpServlet {
     out.println("<html>");
     out.println("<head><meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
     out.println("<title>Session Test Servlet</title></head><body>");
-    out.println("<h2 style=\"color:blue;\">Request from: " + requestURL + " </h2>");
+    if (trustedPages.contains(requestedPage)) {
+      out.println("<h2 style=\"color:blue;\">Request from " + requestedPage + " page</h2>");
+    } else {
+      out.println("<h2 style=\"color:red;\">Request from untrusted page!</h2>");
+    }
     out.println("</body></html>");
     out.flush();
     out.close();
--- task2/src/main/test/com.clouway/ServletRecognizerTest.java
+++ task2/src/main/test/com.clouway/ServletRecognizerTest.java
@@ -1,41 +1,73 @@
 package com.clouway;
 
-import org.junit.Before;
+import org.jmock.Expectations;
+import org.jmock.auto.Mock;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Rule;
 import org.junit.Test;
 
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayOutputStream;
+import java.io.PrintWriter;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
 
 /**
- * Created by com.clouway on 16.05.16.
+ * Created by clouway on 17.05.16.
  */
 public class ServletRecognizerTest {
+  @Rule
+  public JUnitRuleMockery context = new JUnitRuleMockery();
 
-  private FakeRequest fakeRequest;
-  private FakeResponse fakeResponse;
-  private RecognizerServlet recognizerServlet;
+  @Mock
+  HttpServletRequest request;
 
+  @Mock
+  HttpServletResponse response;
 
-  @Before
-  public void setUp(){
-    recognizerServlet=new RecognizerServlet();
-    fakeRequest=new FakeRequest();
-    fakeResponse = new FakeResponse();
+  @Test
+  public void requestFromTrustedPage() throws Exception {
+    final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    RecognizerServlet recognizerServlet = new RecognizerServlet();
+
+    final String page = "Abv";
+
+    context.checking(new Expectations() {{
+      oneOf(request).getParameter("page");
+      will(returnValue(page));
+      oneOf(response).getWriter();
+      will(returnValue(new PrintWriter(out)));
+    }});
+
+    recognizerServlet.doGet(request, response);
+
+    String actual = out.toString();
+
+    assertThat(actual, containsString("Request from Abv page"));
   }
 
   @Test
-  public void checkRequest() throws Exception {
+  public void requestFromUntrustedPage() throws Exception {
     final ByteArrayOutputStream out = new ByteArrayOutputStream();
 
-    fakeRequest.setHeader("Referer","http://localhost:8080/pages/abv.html");
-    fakeResponse.setOutputStream(out);
-    recognizerServlet.doGet(fakeRequest,fakeResponse);
+    RecognizerServlet recognizerServlet = new RecognizerServlet();
 
-    String expected = out.toString();
+    final String page = "CNN";
 
-    assertThat(expected.contains("http://localhost:8080/pages/abv.html"),is(true));
+    context.checking(new Expectations() {{
+      oneOf(request).getParameter("page");
+      will(returnValue(page));
+      oneOf(response).getWriter();
+      will(returnValue(new PrintWriter(out)));
+    }});
+
+    recognizerServlet.doGet(request, response);
+
+    String actual = out.toString();
+
+    assertThat(actual, containsString("Request from untrusted page!"));
   }
 }
