29c7468bf2c80f2787003a181109fd8b5abddeaa
Stanislava Kaukova <sisiivanovva@gmail.com>
2016-07-06 09:09:20 +0300

    servlets: login validation
    added validation for email and password

--- src/main/java/com/clouway/bank/adapter/http/LoginControllerServlet.java
+++ src/main/java/com/clouway/bank/adapter/http/LoginControllerServlet.java
@@ -1,7 +1,17 @@
 package com.clouway.bank.adapter.http;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @author Stanislava Kaukova(sisiivanovva@gmail.com)
  */
-public class LoginControllerServlet {
+public class LoginControllerServlet extends HttpServlet {
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    
+  }
 }
