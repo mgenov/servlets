7bf2f2d11e73f8b84122a0c4219aaadd2fdb8521
Stanislava Kaukova <sisiivanovva@gmail.com>
2016-07-06 09:00:15 +0300

    servlets: login
    added LoginPageServlet, LoginControllerServlet

---
+++ src/main/java/com/clouway/bank/adapter/http/LoginControllerServlet.java
@@ -0,0 +1,7 @@
+package com.clouway.bank.adapter.http;
+
+/**
+ * @author Stanislava Kaukova(sisiivanovva@gmail.com)
+ */
+public class LoginControllerServlet {
+}
--- src/main/java/com/clouway/bank/adapter/server/jetty/Jetty.java
+++ src/main/java/com/clouway/bank/adapter/server/jetty/Jetty.java
@@ -1,8 +1,8 @@
 package com.clouway.bank.adapter.server.jetty;
 
-import com.clouway.bank.adapter.http.LoginServlet;
-import com.clouway.bank.adapter.jdbc.ConnectionProvider;
+import com.clouway.bank.adapter.http.LoginPageServlet;
 import com.clouway.bank.adapter.http.RegisterServlet;
+import com.clouway.bank.adapter.jdbc.ConnectionProvider;
 import com.clouway.bank.adapter.jdbc.db.persistence.PersistentUserRepository;
 import com.clouway.bank.validator.UserValidator;
 import org.eclipse.jetty.server.Server;
@@ -30,7 +30,8 @@ public class Jetty {
       public void contextInitialized(final ServletContextEvent servletContextEvent) {
         ServletContext servletContext = servletContextEvent.getServletContext();
         servletContext.addServlet("register", new RegisterServlet(new PersistentUserRepository(new ConnectionProvider("jdbc:postgresql://localhost/bank", "postgres", "clouway.com")), new UserValidator())).addMapping("/register");
-        servletContext.addServlet("login", new LoginServlet()).addMapping("/login");
+        servletContext.addServlet("login", new LoginPageServlet()).addMapping("/login");
+        servletContext.addServlet("loginController", new LoginPageServlet()).addMapping("/loginController");
       }
 
       public void contextDestroyed(ServletContextEvent servletContextEvent) {
