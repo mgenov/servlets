ae0fcd0903b876031bba51eed07365024b54a37a
Stanislava Kaukova <sisiivanovva@gmail.com>
2016-07-11 20:06:58 +0300

    servlets: filter
    added check if user can login only once

--- src/main/java/com/clouway/bank/adapter/http/SecurityFilter.java
+++ src/main/java/com/clouway/bank/adapter/http/SecurityFilter.java
@@ -14,49 +14,49 @@ import java.io.IOException;
  * @author Stanislava Kaukova(sisiivanovva@gmail.com)
  */
 public class SecurityFilter implements Filter {
-  private final SessionRepository sessionRepository;
+    private final SessionRepository sessionRepository;
 
-  public SecurityFilter(SessionRepository sessionRepository) {
-    this.sessionRepository = sessionRepository;
-  }
-
-  @Override
-  public void init(FilterConfig filterConfig) throws ServletException {
-
-  }
-
-  @Override
-  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-    HttpServletRequest request = (HttpServletRequest) servletRequest;
-    HttpServletResponse response = (HttpServletResponse) servletResponse;
-
-    Cookie cookie = find(request.getCookies());
-
-    final String uri = request.getRequestURI();
-
-    Optional<Session> currentUser = null;
-    if (cookie != null) {
-      currentUser = sessionRepository.findSessionById(cookie.getValue());
+    public SecurityFilter(SessionRepository sessionRepository) {
+        this.sessionRepository = sessionRepository;
     }
-    if (uri.contains("/login") || currentUser.isPresent()) {
-      filterChain.doFilter(request, response);
 
-    } else {
-      response.sendRedirect("/login");
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
     }
-  }
 
-  @Override
-  public void destroy() {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
 
-  }
+        final String uri = request.getRequestURI();
 
-  private Cookie find(Cookie[] cookies) {
-    for (Cookie each : cookies) {
-      if (each.getName().equals("id")) {
-        return each;
-      }
+        String sessionId = find(request.getCookies());
+        Optional<Session> currentUser = sessionRepository.findSessionById(sessionId);
+        if (uri.contains("/login") && currentUser.isPresent()) {
+            response.sendRedirect("/home");
+        }
+        if (uri.contains("/login") || currentUser.isPresent()) {
+            filterChain.doFilter(request, response);
+        } else {
+            response.sendRedirect("/login");
+        }
     }
-    return null;
-  }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    private String find(Cookie[] cookies) {
+        String sessionId = "";
+        for (Cookie each : cookies) {
+            Cookie cookie = each;
+            if (cookie.getName().equals("id")) {
+                sessionId = cookie.getValue();
+            }
+        }
+        return sessionId;
+    }
 }
